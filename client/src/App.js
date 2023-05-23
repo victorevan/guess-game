71fde3ff2ef1d90288cf5f09ec95c8f3af6c3b0f
Yibran <yibran.enrique@gmail.com>
2023-05-22 21:34:01 -0700

    Join/Lobby
    Client side and server side can communicate now. Still working on the joining and Creating lobbies.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import { useState } from 'react';
+
+const socket = io('http://localhost:3001');
 
 function App() {
+  const [name, setName] = useState('');
+  const [creatingRoom, isCreatingRoom] = useState("");
+  const [joiningRoom, isJoiningRoom] = useState("");
+
+  const joinRoom = () => {
+    if (name !== "" && joiningRoom === "") {
+      socket.emit("join_room", joiningRoom);
+    }
+  };
+
+  const createRoom = () => {
+    if (name !== "" && createRoom === "") {
+      socket.emit("create_room", creatingRoom);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">Guess Game
+      <form className='Menu'>
+        <input 
+        type="text" 
+        placeholder="Enter your name"
+        value={name}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+        />
+        <button type="submit" onClick={joinRoom}>Join Game</button>
+        <button type="submit" onClick={createRoom}>Create Game</button>
+      </form>
+      <div>
+        <h1>About</h1>
+      </div>
       </header>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,6 +1,63 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const http = require('http');
+const cors = require('cors');
+const port = 3001;
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST"],
+  },
+});
+
+const rooms = {};
+
+// create a room
+io.on('createRoom', (roomName, callback) => {
+  const room = {
+    id: uuid(), // generate a unique id for the new room, that way we don't need to deal with duplicates.
+    name: roomName,
+    sockets: []
+  };
+  rooms[room.id] = room;
+  // have the socket join the room they've just created.
+  joinRoom(socket, room);
+  callback();
+});
+
+// when player joins a room
+io.on ("joinRoom", (roomName, callback) => {
+  const room = room[roomName];
+  joinRoom(socket, room);
+  callback();
+});
+
+// When player leaves the room
+io.on("leaveRoom", (callback) => {
+  leaveRoom(socket);
+  callback();
+});
+
+// Players connection to the server
+io.on("connection", (socket) => {
+  console.log(`connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  })
+
+  socket.on("disconnect", () => {
+    console.log(`disconnect: ${socket.id}`);
+  });
+});
 
 // Define a route
 app.get('/', (req, res) => {
@@ -8,6 +65,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
 });
